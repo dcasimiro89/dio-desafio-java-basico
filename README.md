94843256f31138c2533cefdbd769ea3e28edf85b
Deivison Savio Casimiro <deivison.savio@gmail.com>
2023-08-29 22:22:05 -0300

    Criado o arquivo Conta Terminal e README atualizado

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # dio-desafio-java-basico
-Desafio proposto pela DIO / Santander 2023
+:arrow_forward: Desafio proposto pela DIO / Santander 2023
+<hr>
+
+<ul>
+    <li>
+        Projeto básico para demonstrar a captura de entrada, utilizando a classe Scanner + String (concatenações).
+    </li>
+    <li>
+        Utilizado método com parâmetros, para a impressão dos dados armazenados.
+    </li>
+</ul>
--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,8 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello, World!");
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = sc.next();
+        System.out.print("Número da Conta: ");
+        int numero = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = sc.nextLine().toUpperCase();
+        System.out.print("Saldo: ");
+        double saldo = sc.nextDouble();
+
+        String imprimir = imprimir(agencia, numero, nomeCliente, saldo);
+        System.out.println(imprimir);
+
+        sc.close();
+
     }
+
+    public static String imprimir(String agencia, int numero, String nome, double saldo) {
+        return "Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+    }
+
+    /* método sem retorno
+    public static void imprimir(String agencia, int numero, String nome, double saldo) {
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+    */
+    
 }
